e7e27661866d10c3eae9983553da609697ff92a0
Andrey <vsenetak88@mail.ru>
2017-05-24 13:40:48 +0000

    init project

---
+++ gulp-tasks/js_tasks.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp'),
+    merge = require('merge-stream'),
+    del = require('del'),
+    file = require('gulp-file'),
+    plugins = require("gulp-load-plugins")({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /\bgulp[\-.]/
+    });
+    
+var resources = {},
+    paths = {};
+
+paths.bowerComponents = './bower_components/';    
+
+
+resources.bowerJs = [
+    paths.bowerComponents + "jquery/dist/jquery.js",
+    paths.bowerComponents + 'jquery.nicescroll/jquery.nicescroll.min.js',
+    
+];
+
+gulp.task('clean:js:assets', function () {
+    return del.sync(['dist/assets/js/assets.min.js']);
+});
+
+gulp.task('js:assets', ['clean:js:assets'], function () {
+    
+    return  gulp.src(resources.bowerJs)
+            .pipe(plugins.concat('source/vendor/js/**/*.js'))
+            
+        .pipe(plugins.concat('assets.js'))
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest('dist/assets/js/'));
+        
+});
+
+gulp.task('clean:js:app', function () {
+    return del.sync(['dist/assets/js/main-app.js']);
+});
+
+gulp.task('js:app', ['clean:js:app'], function() {
+    console.log('resources.bowerJs', resources.bowerJs);
+
+    return gulp.src('source/custom/js/**/*js')
+        .pipe(plugins.concat('main-app.js'))
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest('dist/assets/js/'));
+});
